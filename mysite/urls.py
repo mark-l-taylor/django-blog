61f8a479f98103bb7e4ccfe50dee905b1ff3f4f1
Mark L Taylor <taylormarkld@gmail.com>
2021-04-15 20:48:34 -0700

    Completed the lesson 06 setup of the polling and blogging applications.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # <-- Make sure you have both of these imports.
 
 urlpatterns = [
+    path('polling/', include('polling.urls')),  # <-- Add this
     path('admin/', admin.site.urls),
 ]
